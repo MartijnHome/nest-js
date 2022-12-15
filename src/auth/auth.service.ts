8d61e48357ba7bcea4fce42876eca15600118fb7
Martijn van Sliedregt <>
2022-12-15 13:45:38 +0100

    Initial commit

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable({})
+export class AuthService {
+  signup() {
+    return {
+      msg: 'SignUp',
+    };
+  }
+
+  signin() {
+    return {
+      msg: 'SignIn',
+    };
+  }
+}
